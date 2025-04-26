d5bf2456f0b5f85d70a4402b020c2ed87863c5f4
adityawintech <aditya#19AI>
2025-04-26 10:22:40 +0530

    Aditya

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,15 +1,15 @@
-import PortfolioPageHero from "@/components/PortfolioPage/PortfolioPageHero/PortfolioPageHero"
-import PortfolioPageProjects from "@/components/PortfolioPage/PortfolioPageProjects/PortfolioPageProjects"
-import PortfolioPageTestimonials from "@/components/PortfolioPage/PortfolioPageTestimonials/PortfolioPageTestimonials"
-import PortfolioPageCTA from "@/components/PortfolioPage/PortfolioPageCTA/PortfolioPageCTA"
+// import PortfolioPageHero from "@/components/PortfolioPage/PortfolioPageHero/PortfolioPageHero"
+// import PortfolioPageProjects from "@/components/PortfolioPage/PortfolioPageProjects/PortfolioPageProjects"
+// import PortfolioPageTestimonials from "@/components/PortfolioPage/PortfolioPageTestimonials/PortfolioPageTestimonials"
+// import PortfolioPageCTA from "@/components/PortfolioPage/PortfolioPageCTA/PortfolioPageCTA"
 
 export default function Portfolio() {
   return (
     <>
-      <PortfolioPageHero />
+      {/* <PortfolioPageHero />
       <PortfolioPageProjects />
       <PortfolioPageTestimonials />
-      <PortfolioPageCTA />
+      <PortfolioPageCTA /> */}
     </>
   )
 }
